bcd6014186469bcd2db1d8d49318d8c900008daa
ViniciusOshima <vinicius_hideaki@outlook.com>
2023-06-15 11:47:33 -0300

    FEAT introdução do projeto com configurações iniciais, como temas e suas tipagens, estilizações globais, integração do eslint

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,4 +6,18 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  body {
+    background: ${(props) => props.theme.background};
+  }
+
+  h1, h2, h3 {
+    font-family: 'Baloo 2', cursive;
+  }
+
+  p {
+    font-family: 'Roboto', sans-serif;
+    color: ${(props) => props.theme['base-text']};
+    font-size: 1rem;
+  }
 `
