0e99e747a142737043b7b9c79bacfea539f68270
ViniciusOshima <vinicius_hideaki@outlook.com>
2023-08-21 20:21:25 -0300

    FEAT deploy

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,7 +8,7 @@ export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Home />} />
+        <Route path="/Coffee-Delivery" element={<Home />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/success" element={<Success />} />
       </Route>
